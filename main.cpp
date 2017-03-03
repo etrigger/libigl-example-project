f891af5e9ebc78f176da7048349acb42c1e30471
etrigger <triggerliu@gmail.com>
2017-03-03 11:03:49 +0100

    showing skeleton model

--- main.cpp
+++ main.cpp
@@ -1,34 +1,57 @@
+#include <igl/readOFF.h>
+#include <igl/readTGF.h>
 #include <igl/viewer/Viewer.h>
+#include "shared_path.h"
+
+
+#ifndef TETLIBRARY
+#  define TETLIBRARY 
+#endif
+#include <igl/copyleft/tetgen/read_into_tetgenio.h>
+#include <igl/copyleft/tetgen/tetgenio_to_tetmesh.h>
+#include "igl/boundary_facets.h"
+
+
+
+
+Eigen::MatrixXd V, C;
+
+Eigen::MatrixXi F, BE, T;
+
+const Eigen::RowVector3d sea_green(70. / 255., 252. / 255., 167. / 255.);
+
 
 int main(int argc, char *argv[])
 {
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    1,7,5,
-    1,3,7,
-    1,4,3,
-    1,2,4,
-    3,8,7,
-    3,4,8,
-    5,7,8,
-    5,8,6,
-    1,5,6,
-    1,6,2,
-    2,6,8,
-    2,8,4).finished().array()-1;
+	// Load a mesh in OFF format
+	igl::readOFF(SHARED_PATH "/bunny.off", V, F);
+	std::string skel_filename = "../shared/roboptim-debug-graph-edge.tgf";
+	igl::readTGF(skel_filename, C, BE);
 
-  // Plot the mesh
-  igl::viewer::Viewer viewer;
-  viewer.data.set_mesh(V, F);
-  viewer.data.set_face_based(true);
-  viewer.launch();
+	
+
+	// Plot the mesh
+	igl::viewer::Viewer viewer;
+	//viewer.data.set_mesh(V, F);
+	viewer.data.set_vertices(C);
+
+	double HI = 1; // set HI and LO according to your problem.
+	double LO = 0;
+	double range = HI - LO;
+	Eigen::MatrixX3d m = Eigen::MatrixX3d::Random(C.rows(), 3); // 3x3 Matrix filled with random numbers between (-1,1)
+	int rows = m.rows();
+	int cols = m.cols();
+	m = (m + Eigen::MatrixX3d::Constant(rows, cols, 1.))*range / 2.; // add 1 to the matrix to have values between 0 and 2; multiply with range/2
+	m = (m + Eigen::MatrixX3d::Constant(rows, cols, LO)); //set LO as the lower bound (offset)
+
+	const Eigen::MatrixX3d color = m;
+	//viewer.data.set_points(C, color);
+	viewer.data.set_edges(C, BE, sea_green);
+	
+	
+
+	viewer.core.point_size = 10;
+	//viewer.core.show_faces = false;
+	//viewer.core.line_width = 30;
+	viewer.launch();
 }
---
+++ shared_path.h
@@ -0,0 +1,8 @@
+#ifndef shared_path_h_included
+#define shared_path_h_included
+
+#ifndef SHARED_PATH
+#define SHARED_PATH "../shared"
+#endif
+
+#endif
